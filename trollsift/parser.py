fad4a0fb1168b23fe584bcd97c795611f3905243
Panu Lahtinen <pnuu+git@iki.fi>
2014-06-10 15:06:43 +0300

    Implemented _extract_parsedef().

--- trollsift/parser.py
+++ trollsift/parser.py
@@ -1,7 +1,26 @@
 
+import re
 
-def _extract_parsedef( fmt ):
-    return []
+def _extract_parsedef(fmt):
+    '''Retrieve parse definition from the format string *fmt*.
+    '''
+
+    parsedef = []
+
+    for part1 in fmt.split('}'):
+        for part2 in part1.split('{'):
+            if part2 is not '':
+                if ':' in part2:
+                    part2 = part2.split(':')
+                    parsedef.append({part2[0]: part2[1]})
+                else:
+                    reg = re.search('(\{'+part2+'\})', fmt)
+                    if reg:
+                        parsedef.append({part2: None})
+                    else:
+                        parsedef.append(part2)
+
+    return parsedef
 
 
 def _extract_values( parsedef, stri):
