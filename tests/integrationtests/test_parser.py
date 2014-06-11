bf9c6c3714737631a867d1b98356b5f466eed926
Panu Lahtinen <pnuu+git@iki.fi>
2014-06-11 14:50:08 +0300

    Fixed typos in tests

--- tests/integrationtests/test_parser.py
+++ tests/integrationtests/test_parser.py
@@ -9,26 +9,27 @@ class TestParser(unittest.TestCase):
         self.fmt = "/somedir/{directory}/hrpt_{platform:4s}{platnum:2s}" +\
             "_{time:%Y%m%d_%H%M}_{orbit:05d}.l1b"
         self.string = "/somedir/otherdir/hrpt_noaa16_20140210_1004_69022.l1b"
-        self.data = {'directory': 'avhrr/2014', 'platform': 'noaa', 'platnum': '19',
-                     'time': dt.datetime(2014,02,12,14,12), 'orbit':12345}
+        self.data = {'directory': 'otherdir', 'platform': 'noaa',
+                     'platnum': '16',
+                     'time': dt.datetime(2014,02,10,10,4), 'orbit': 69022}
         self.p = Parser(self.fmt)
 
     def test_parse(self):
         # Run
-        result = p.parse(self.string)
+        result = self.p.parse(self.string)
         # Assert
         self.assertDictEqual(result, self.data)
 
     def test_compose(self):
         # Run
-        result = p.compose(self.data)
+        result = self.p.compose(self.data)
         # Assert
-        self.assertDictEqual(result, self.string)
+        self.assertEqual(result, self.string)
         
     def assertDictEqual(self, a, b):
         for key in a:
             self.assertTrue( key in b )
-            self.assertEqual(a[key],b[key])
+            self.assertEqual(a[key], b[key])
 
         self.assertEqual(len(a), len(b))
 
