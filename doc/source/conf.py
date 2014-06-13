779e2f74294bc67155f0576c943330a6aee83471
Panu Lahtinen <pnuu+git@iki.fi>
2014-06-13 13:39:12 +0300

    More syntactic cleanup

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -96,7 +96,7 @@ html_theme = 'default'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+# html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
@@ -227,10 +227,11 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'trollsift', u'Trollsift Documentation', u'Panu Lahtinen',
-   u'Hrobjartur Thorsteinsson', 'trollsift', 'One line description of project.',
-   'Miscellaneous'),
-]
+    ('index', 'trollsift', u'Trollsift Documentation', u'Panu Lahtinen',
+     u'Hrobjartur Thorsteinsson', 'trollsift',
+     'One line description of project.',
+     'Miscellaneous'),
+    ]
 
 # Documents to append as an appendix to all manuals.
 # texinfo_appendices = []
