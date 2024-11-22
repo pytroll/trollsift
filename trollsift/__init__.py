907b468ce1456e4d44016f11e3e5b4064333daf6
Stephan Finkensieper <stephan.finkensieper@dwd.de>
2024-11-22 11:43:36 +0000

    Exlude coverage miss

--- trollsift/__init__.py
+++ trollsift/__init__.py
@@ -3,7 +3,7 @@ from .parser import *
 
 try:
     from trollsift.version import version as __version__  # noqa
-except ModuleNotFoundError:
+except ModuleNotFoundError:  # pragma: no cover
     raise ModuleNotFoundError(
         "No module named trollsift.version. This could mean "
         "you didn't install 'trollsift' properly. Try reinstalling ('pip "
